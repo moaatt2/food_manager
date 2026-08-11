b00766c048e00947c01dd0cc384c98dbf3b1bb46
Dakota McKay <dakotamckay72@gmail.com>
2026-08-11 17:16:05 -0400

    Make ORM of basic columns of meals table and run an ORM query against the database.

--- api/api.py
+++ api/api.py
@@ -1,16 +1,39 @@
-import sqlalchemy
 
-# Connect to database and create connection
-engine = sqlalchemy.create_engine("postgresql://test:test@localhost:5433/food")
-with engine.connect() as connection:
+from datetime import datetime
+from typing import List, Optional
+from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy import select, create_engine
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase, Session
 
-    # Begin connection for safety
-    connection.begin()
+# Base Class
+class Base(DeclarativeBase):
+    pass
 
-    # Run query on database and print results
-    cursor = connection.exec_driver_sql("SELECT * FROM INFORMATION_SCHEMA.TABLES LIMIT 10;")
-    for row in cursor._allrows():
-        print(row)
 
-    # Ensure no changes remain
-    connection.rollback()
+class Meal(Base):
+    __tablename__ = "meals"
+    __table_args__ = {"schema": "food"}
+
+    id:         Mapped[int] = mapped_column(primary_key=True)
+    name:       Mapped[str]
+    source:     Mapped[str]
+    num_meals:  Mapped[int]
+    keeps_days: Mapped[int]
+    created_at: Mapped[datetime]
+    updated_at: Mapped[datetime]
+    deleted_at: Mapped[Optional[datetime]]
+
+    def __repr__(self):
+        return f"Meal(id={self.id!r}, name={self.name!r})"
+
+# Create Engine/Session
+engine = create_engine("postgresql://test:test@localhost:5433/food")
+with Session(engine) as session:
+
+    stmt = select(Meal).where(Meal.id == 1)
+
+    for meal in session.scalars(stmt):
+        print(meal)
+
+
+
